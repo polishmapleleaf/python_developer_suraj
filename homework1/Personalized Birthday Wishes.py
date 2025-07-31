8e25a47f2af61410c08b7c2a6e036bd5ff674bf6
suraj <you@example.com>
2025-07-31 18:20:34 +0200

    tuples

---
+++ homework1/Personalized Birthday Wishes.py
@@ -0,0 +1,9 @@
+sender_name = input("enter your name ")
+receipient_name = input("enter your receipient name ")
+year_of_birth = int(input(f"In which year your {receipient_name} is born? "))
+age = 2025-year_of_birth
+print(f"\n \n {receipient_name}, let's celebrate your {age} years of awesomeness! \n"
+      f"Wishing you a day filled with joy and laughter as you turn {age}! \n"
+      f"god will bless you {receipient_name} \n"
+      f"With love and best wishes,\n"
+      f"{sender_name} ")
